7c7a327c9f775ae89aec4f06bde07e17d0bcb9a4
anal <kondratjeb2@gmail.com>
2023-02-27 23:25:34 +0300

    third commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import { VscGithubAlt } from "react-icons/vsc"
+
+function Navbar() {
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <a href="/#">Заметки</a>
+        <a href="/#">Напоминания</a>
+        <a href="/#">Изменение ярлыков</a>
+        <a href="/#">Архив</a>
+        <a href="/#">Корзина</a>
+        <button></button>
+      </nav>
+      <button>
+        
+      </button>
+    </header>
+  )
+}
+
+export default Navbar
